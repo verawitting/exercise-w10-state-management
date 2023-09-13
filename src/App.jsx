aae16aafb867ab1665d1a9ccbfe4c461c2a94cef
Diego Zito <diegozito.m@gmail.com>
2023-09-13 16:40:19 +0200

    readMe

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,3 @@
-// Import required  hooks
-import { DogFact } from "./components/DogFact";
-
 export const App = () => {
-  // Hint: Initialize state for storing the dog fact
-  // Hint: Define the API endpoint
-  // Hint: Create a function to fetch the dog fact
-  // Hint: Use the useEffect hook to fetch the dog fact when the component mounts
-
-  return (
-    <div className="App">
-      <DogFact />
-    </div>
-  );
+  return <div className="App">Hello Coders!</div>;
 };
